3bf08e73b0645b8f2610e3e58d006fff5491a0b0
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2018-03-29 15:47:25 +0200

    Changed singularity image names to real command names

--- init_modules.sh
+++ init_modules.sh
@@ -1,10 +1,10 @@
 MODULE_PACKAGES="/packages"
 MODULE_FILES="/usr/share/modules/modulefiles/"
-while IFS="	" read -r name version image remainder
+while IFS="	" read -r name version image command remainder
 do
     mkdir -p $MODULE_PACKAGES/$name/$version
     singularity pull --name tmp.img $image
-    mv tmp.img $MODULE_PACKAGES/$name/$version/$name
+    mv tmp.img $MODULE_PACKAGES/$name/$version/$command
     mkdir -p $MODULE_FILES/$name/
     cat > $MODULE_FILES/$name/$version <<EOF
 #%Module1.0
