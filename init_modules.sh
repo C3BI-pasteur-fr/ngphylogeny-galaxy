b23364ae5830834cf3a56543c237bb4d4a73f6b5
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2018-03-29 17:28:42 +0200

    Several commands per singularity images

--- init_modules.sh
+++ init_modules.sh
@@ -1,10 +1,15 @@
 MODULE_PACKAGES="/packages"
 MODULE_FILES="/usr/share/modules/modulefiles/"
-while IFS="	" read -r name version image command remainder
+while IFS="	" read -r name version image commands remainder
 do
     mkdir -p $MODULE_PACKAGES/$name/$version
     singularity pull --name tmp.img $image
-    mv tmp.img $MODULE_PACKAGES/$name/$version/$command
+    mv tmp.img $MODULE_PACKAGES/$name/$version/${name}.img
+    for c in $(echo $commands | sed "s/,/ /g")
+    do
+	echo "#!/bin/sh\nsingularity exec $MODULE_PACKAGES/$name/$version/${name}.img $c \${1+\"\$@\"}\n" > $MODULE_PACKAGES/$name/$version/$c
+	chmod +x $MODULE_PACKAGES/$name/$version/$c
+    done
     mkdir -p $MODULE_FILES/$name/
     cat > $MODULE_FILES/$name/$version <<EOF
 #%Module1.0
