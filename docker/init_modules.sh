760bbe4843a866967a4f313ed7293b80df34915f
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2019-01-25 16:38:11 +0100

    Install parallel

--- docker/init_modules.sh
+++ docker/init_modules.sh
@@ -25,3 +25,25 @@ prepend-path PATH $MODULE_PACKAGES/$name/$version/
 EOF
 
 done < "package_list.txt"
+
+# Install Parallel in Module
+wget ftp://ftp.gnu.org/gnu/parallel/parallel-20170122.tar.bz2
+tar -xjvf parallel-20170122.tar.bz2
+cd parallel-20170122
+./configure --prefix=$MODULE_PACKAGES/parallel/20170122
+make
+make install
+cd ..
+rm -rf parallel-20170122.tar.bz2 parallel-20170122
+mkdir -p $MODULE_FILES/parallel
+cat > $MODULE_FILES/parallel/20170122 <<EOF
+#%Module1.0
+proc ModulesHelp { } {
+global dotversion
+ 
+puts stderr "\t$name $version"
+}
+ 
+module-whatis "$name $version"
+prepend-path PATH $MODULE_PACKAGES/parallel/20170122/bin/
+EOF
